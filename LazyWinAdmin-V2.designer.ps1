08695b2db03e5161948bfacaedcdb8d59f1779d6
MinersWin <minerswin@minerswin.de>
2020-06-24 23:22:17 +0200

    Changed Windows Size to 1024x720

--- LazyWinAdmin-V2.designer.ps1
+++ LazyWinAdmin-V2.designer.ps1
@@ -5,7 +5,7 @@ $Form1.SuspendLayout()
 #
 #Form1
 #
-$Form1.ClientSize = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]947,[System.Int32]539))
+$Form1.ClientSize = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]1008,[System.Int32]681))
 $Form1.Text = [System.String]'Form1'
 $Form1.ResumeLayout($false)
 Add-Member -InputObject $Form1 -Name base -Value $base -MemberType NoteProperty
