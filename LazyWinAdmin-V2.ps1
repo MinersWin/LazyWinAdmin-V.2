ed25d690717cee90e81dbcae83b929f88e98ba8e
Moritz Mantel <moritz.mantel@basecamp.fusic.de>
2020-06-26 16:39:49 +0200

    Dont use, just Testing

--- LazyWinAdmin-V2.ps1
+++ LazyWinAdmin-V2.ps1
@@ -1,3 +1,18 @@
 Add-Type -AssemblyName System.Windows.Forms
 . (Join-Path $PSScriptRoot 'LazyWinAdmin-V2.designer.ps1')
+#Bind Functions
+$ButtonCheck.Add_Click{(CheckHostname)}
+
+#Get Variables
+$LocalHostName = $env:computername
+
+#Insert Infos
+$ComboBoxComputerName.Text = $LocalHostName
+
+#Funktions
+function CheckHostname{
+    $SelectedHostName = $ComboBoxComputerName.Text
+    $Connection = Test-Connection $SelectedHostName
+    Write-Host $Connection
+}
 $FormLWA.ShowDialog()
